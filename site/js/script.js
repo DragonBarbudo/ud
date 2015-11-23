7feaa914f12ae6b31135a89829e374519e15a2db
Albin Rdz <ealbinu@gmail.com>
2015-11-23 08:24:56 -0600

    Countdown post

--- site/js/script.js
+++ site/js/script.js
@@ -156,6 +156,16 @@ $('#listscroll').scrollbox({
 });
 
 
+
+if($('[data-countdown]').length>0){
+  $('[data-countdown]').each(function() {
+    var $this = $(this), finalDate = $(this).data('countdown');
+    $this.countdown(finalDate, function(event) {
+    $this.html(event.strftime('%D días %H:%M:%S'));
+    });
+  });
+}
+
 }); // JQUERY END
 
 
